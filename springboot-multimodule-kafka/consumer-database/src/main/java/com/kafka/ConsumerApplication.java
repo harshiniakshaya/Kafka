0c649efc8f6057b74b3181e4cb89aa48bcac84b1
harshiniakshaya <harshiniakshaya@gmail.com>
2026-09-21 01:49:13 +0530

    Set up H2 database and update database with Wikimedia event messages from Kafka topic

--- springboot-multimodule-kafka/consumer-database/src/main/java/com/kafka/ConsumerApplication.java
+++ springboot-multimodule-kafka/consumer-database/src/main/java/com/kafka/ConsumerApplication.java
@@ -1,13 +1,22 @@
 package com.kafka;
 
+import org.h2.server.web.JakartaWebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class);
+        SpringApplication.run(ConsumerApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean<JakartaWebServlet> h2servletRegistration() {
+        ServletRegistrationBean<JakartaWebServlet> registrationBean = new ServletRegistrationBean<>(new JakartaWebServlet());
+        registrationBean.addUrlMappings("/h2-console/*");
+        return registrationBean;
+    }
 }
---
+++ springboot-multimodule-kafka/consumer-database/src/main/java/com/kafka/repository/WikimediaDataRepository.java
@@ -0,0 +1,8 @@
+package com.kafka.repository;
+
+import com.kafka.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+
+}
